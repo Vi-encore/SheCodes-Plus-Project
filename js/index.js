9c33d5bdf6351ca292bfcae2ff8618b9c8d76a48
Vi <vikamatuchko772@gmail.com>
2023-01-12 22:29:55 +0200

    something

--- js/index.js
+++ js/index.js
@@ -90,21 +90,24 @@ function changeDate() {
   let currentYear = document.getElementById("forecast-year");
   currentYear.innerHTML = year;
 
-  let nextDay1 = document.querySelector("#side__day--1");
-  let nextDay2 = document.querySelector("#side__day--2");
-  let nextDay3 = document.querySelector("#side__day--3");
-  let nextDay4 = document.querySelector("#side__day--4");
+  // let nextDate1 = document.querySelector("#side__date--1");
+  // let nextDate2 = document.querySelector("#side__date--2");
+  // let nextDate3 = document.querySelector("#side__date--3");
+  // let nextDate4 = document.querySelector("#side__date--4");
 
-  let changeDay1 = days[day + 1];
-  let changeDay2 = days[day + 2];
-  let changeDay3 = days[day + 3];
-  let changeDay4 = days[day + 4];
+  // let changeDate1 = date.getDate() + 1;
+  // console.log(changeDate1);
+  // changeDate1 < 10 ? (changeDate = `0${changeDate}`) : changeDate;
+  // nextDate1.innerHTML = changeDate1;
+
+  // let changeDate1 = date.getDate() + 2;
+  // changeDate2 < 10 ? (changeDate = `0${changeDate}`) : changeDate;
+  // nextDate1.innerHTML = changeDate1;
 
   // nextDay1.innerHTML = changeDay1;
   // nextDay2.innerHTML = changeDay2;
   // nextDay3.innerHTML = changeDay3;
   // nextDay4.innerHTML = changeDay4;
-
 }
 
 changeDate();
@@ -315,4 +318,3 @@ function tempCel(event) {
 
 let tempCelc = document.getElementById("forecast--cels");
 tempCelc.addEventListener("click", tempCel);
-
